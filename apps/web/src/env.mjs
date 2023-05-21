686ea4e71b90b1d104e726d24468470013e9fb18
James Brinkerhoff <th3brink@gmail.com>
2023-05-21 15:02:38 -0600

    testing sripe webhook 8

--- apps/web/src/env.mjs
+++ apps/web/src/env.mjs
@@ -11,6 +11,7 @@ const server = z.object({
   NEXT_PUBLIC_SUPABASE_ANON_KEY: z.string().min(1),
   STRIPE_WEBHOOK_SECRET: z.string().min(1),
   SUPABASE_SERVICE_ROLE_KEY: z.string().min(1),
+  SUPABASE_SERVICE_ROLE_KEY: z.string().min(1),
 })
 
 /**
--- apps/web/src/utils/api.ts
+++ apps/web/src/utils/api.ts
@@ -9,7 +9,7 @@ import { supabaseClient } from './database'
 
 const getBaseUrl = () => {
   if (typeof window !== 'undefined') return '' // browser should use relative url
-  if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}` // SSR should use vercel url
+  if (process.env.VERCEL_URL) return `${process.env.VERCEL_URL}` // SSR should use vercel url
 
   return `http://localhost:3000` // dev SSR should use localhost
 }
